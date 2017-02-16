b641c7f3c5a7e57839ecbe4a58515e1641b8bdad
Kundjanasith Thonglek <kundjanasiththonglek@Kundjanasiths-MacBook-Air.local>
2017-02-16 10:55:46 +0700

    week4

---
+++ week_4.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var arrs = ["Hello","My Name","Jitti"]
+
+arrs[0]
+arrs[1]
+arrs[2]
+
+arrs.count
+
+
+
